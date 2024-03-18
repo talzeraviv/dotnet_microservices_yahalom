e4b977378b890805a7bcce49faadb0402d90e606
Tal Zer-Aviv <106658389+talzeraviv@users.noreply.github.com>
2024-03-18 19:31:03 +0200

    Add model validation for Dtos.cs

--- src/Play.Catalog.Service/Dtos.cs
+++ src/Play.Catalog.Service/Dtos.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Play.Catalog.Service.Dtos
 {
     public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreateDate);
-    public record CreateItemDto(string Name, string Description, decimal Price);
-    public record UpdateItemDto(string Name, string Description, decimal Price);
+    public record CreateItemDto([Required] string Name, string Description, [Range(0, double.MaxValue, ErrorMessage = "The price must be greater than 0 and lower than max value for the double data type.")] decimal Price);
+    public record UpdateItemDto([Required] string Name, string Description, [Range(0, double.MaxValue, ErrorMessage = "The price must be greater than 0 and lower than max value for the double data type.")] decimal Price);
 }
