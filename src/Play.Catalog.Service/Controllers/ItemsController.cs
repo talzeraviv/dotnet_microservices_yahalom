40fbba68615f437453e03103e28d3ee0a19b0bea
Tal Zer-Aviv <106658389+talzeraviv@users.noreply.github.com>
2024-03-18 18:00:26 +0200

    Section 1: Step 8 - Create ItemsController in Controllers folder, Move Dtos to correct folder (Play.Catalog -> Play.Catalog.Service)

---
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Service.Dtos;
+
+namespace Play.Catalog.Service.Controllers
+{
+    // Http://localhost:5241/items
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDto> items = new List<ItemDto>()
+        {
+            new ItemDto(Guid.NewGuid(), "Potion", "Restores a small amount of HP", 5, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Antidote", "Cures Poison", 7, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Bronze Sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow)
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> Get()
+        {
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ItemDto GetById(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }
+}
