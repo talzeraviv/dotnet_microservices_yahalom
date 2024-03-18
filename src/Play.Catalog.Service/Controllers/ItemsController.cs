f67efbca07e58a39c6f70f03135a6d2168785293
Tal Zer-Aviv <106658389+talzeraviv@users.noreply.github.com>
2024-03-18 19:21:32 +0200

    Return NotFound for Get, Post, Delete operations for items that were not found in the db

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -27,9 +27,13 @@ namespace Play.Catalog.Service.Controllers
 
         // GET /items/{id}
         [HttpGet("{id}")]
-        public ItemDto GetById(Guid id)
+        public ActionResult<ItemDto> GetById(Guid id)
         {
-            return items.SingleOrDefault(item => item.Id == id);
+            var item = items.SingleOrDefault(item => item.Id == id);
+
+            if (item == null) return NotFound();
+
+            return item;
         }
 
         // ActionResult allows us to return a status code or an object
@@ -50,7 +54,9 @@ namespace Play.Catalog.Service.Controllers
         [HttpPut("{id}")]
         public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
         {
-            var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
+            var existingItem = items.SingleOrDefault(item => item.Id == id);
+
+            if (existingItem == null) return NotFound();
 
             var updatedItem = existingItem with
             {
@@ -70,6 +76,9 @@ namespace Play.Catalog.Service.Controllers
         public IActionResult Delete(Guid id)
         {
             var index = items.FindIndex(existingItem => existingItem.Id == id);
+
+            if (index < 0) return NotFound();
+
             items.RemoveAt(index);
 
             return NoContent();
